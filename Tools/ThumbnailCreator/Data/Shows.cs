6689d41a5e2c2d46ce0c6788e18c6ad0b3b6260a
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-07-11 17:03:39 +0100

    update shows

--- Tools/ThumbnailCreator/Data/Shows.cs
+++ Tools/ThumbnailCreator/Data/Shows.cs
@@ -1,20 +1,21 @@
-﻿using System.Threading;
+﻿using System.Runtime.Intrinsics.X86;
+using System.Threading;
 
 namespace ThumbnailCreator.Data
 {
     public static partial class Shows
     {
-        public static ShowDetails CenturyVampire()
+        public static ShowDetails PleaseUseOtherDoor()
         {
             ShowDetails showDetails = new ShowDetails();
-            showDetails.Source = @"Images/20 20th Century Vampire.PNG";
-            showDetails.Title = "20th Century Vampire";
+            showDetails.Source = @"Images/21 Please Use Other Door.jpeg";
+            showDetails.Title = "Please Use Other Door";
             showDetails.TitleLine2 = "";
-            showDetails.Path = $@"E:\_AudioTemp\Comedy\_Uploading_B\20 20th Century Vampire\";
-            showDetails.Description = "The adventures of trainee vampire Eloise, her boyfriend Wayne and her eccentric Transylvanian tutor, Lucretia";
+            showDetails.Path = $@"E:\_AudioTemp\Comedy\_Uploading_B\21 Please Use Other Door\";
+            showDetails.Description = "";
             showDetails.ShowTypeLineA = "Radio";
             showDetails.ShowTypeLineB = "Comedy";
-            showDetails.Series = 1;
+            showDetails.Series = 2;
             showDetails.ShortShow = false;
             showDetails.CompleteShow = false;
             showDetails.ComdeyShow = true;
@@ -22,166 +23,51 @@ namespace ThumbnailCreator.Data
 
             EpisodeDetails s01e01 = new EpisodeDetails();
             s01e01.TitleExtra = "";
-            s01e01.Title = "s01e01 I Am a Vampire";
-            s01e01.Description = "Eloise receives monumental news from her mysterious Aunt Lucretia; it seems the Co-op checkout girl is now a vampire!";
+            s01e01.Title = "s01e01 Episode One";
+            s01e01.Description = "A satirical take on modern life. Naturalistic sketches taking a sometimes dark look at the world of work, relationships, institutions and families. Highlights include a goose enrolling as a police officer - and somehow managing to make it through the very tight vetting procedures. There's a character sceptical about time, and a doll-baby so realistic she's more of a nightmare than a toy.";
             showDetails.EpisodeDetails.Add(s01e01);
 
             EpisodeDetails s01e02 = new EpisodeDetails();
             s01e02.TitleExtra = "";
-            s01e02.Title = "s01e02 Back to School";
-            s01e02.Description = "GCSE vampirism exam-time, but at least there is a practical!";
+            s01e02.Title = "s01e02 Episode Two";
+            s01e02.Description = "The second show includes a series of sketches in which someone is forced to watch Succession, a man applies for a business loan for a company whose aim is doing evil and a woman dates the Pied Piper.";
             showDetails.EpisodeDetails.Add(s01e02);
 
             EpisodeDetails s01e03 = new EpisodeDetails();
             s01e03.TitleExtra = "";
-            s01e03.Title = "s01e03 The Biggest Secret";
-            s01e03.Description = "Bursting to share her news of her vampirism, Eloise tells each of her possible confidants a dark secret (all untrue) to see if they can be trusted.";
+            s01e03.Title = "s01e03 Episode Three";
+            s01e03.Description = "We attend the Schadenfreude Experience, there is a visit to a pet shop to buy a giraffe, and why you need a sense of humour to be a heart surgeon.";
             showDetails.EpisodeDetails.Add(s01e03);
 
             EpisodeDetails s01e04 = new EpisodeDetails();
             s01e04.TitleExtra = "";
-            s01e04.Title = "s01e04 Staff Outing";
-            s01e04.Description = "Debbie is responsible for arranging the annual Co-op staff outing, and decides on a trip to donate blood.";
+            s01e04.Title = "s01e04 Episode Four";
+            s01e04.Description = "Some parents tell their daughter the truth about Father Christmas, there is a friendly chat during a rugby scrum, and we witness a polite mugging.";
             showDetails.EpisodeDetails.Add(s01e04);
-
-            EpisodeDetails s01e05 = new EpisodeDetails();
-            s01e05.TitleExtra = "";
-            s01e05.Title = "s01e05 Endangered Species";
-            s01e05.Description = "Eloise and Lucretia discover that Mr Jenkins's blood is the rarest – and tastiest! – in the world.";
-            showDetails.EpisodeDetails.Add(s01e05);
-
-            EpisodeDetails s01e06 = new EpisodeDetails();
-            s01e06.TitleExtra = "";
-            s01e06.Title = "s01e06 Love Shack";
-            s01e06.Description = "Left alone to castle-sit in Lucretia's absence, how far will Eloise let boyfriend Wayne go?";
-            showDetails.EpisodeDetails.Add(s01e06);
-
-            return showDetails;
-        }
-
-        public static ShowDetails PlumHouse()
-        {
-            ShowDetails showDetails = new ShowDetails();
-            showDetails.Source = @"Images/19 Plum House.jpeg";
-            showDetails.Title = "Plum House";
-            showDetails.TitleLine2 = "";
-            showDetails.Path = $@"E:\_AudioTemp\Comedy\_Uploading_B\19 Plum House\";
-            showDetails.Description = "Comedy about the eccentric and inept staff at Plum House, former country home of minor 18th-century poet George Pudding. Written by Ben Cottam and Paul Mckenna.";
-            showDetails.ShowTypeLineA = "Radio";
-            showDetails.ShowTypeLineB = "Comedy";
-            showDetails.Series = 3;
-            showDetails.ShortShow = false;
-            showDetails.CompleteShow = false;
-            showDetails.ComdeyShow = true;
-            showDetails.ScifiShow = false;
-
-            EpisodeDetails s01e01 = new EpisodeDetails();
-            s01e01.TitleExtra = "";
-            s01e01.Title = "s01e01 Why Why Wi?";
-            s01e01.Description = "The museum's preparations for the annual WI visit include hiding away valuable artefacts, as - according to Peter - some members are prone to stealing them. And Julian gives a disastrous lecture on the life and work of George Pudding.";
-            showDetails.EpisodeDetails.Add(s01e01);
-
-            EpisodeDetails s01e02 = new EpisodeDetails();
-            s01e02.TitleExtra = "";
-            s01e02.Title = "s01e02 Perilously Poorly Peter";
-            s01e02.Description = "Peter is forced to visit town. He leaves his hopelessly out-of-touch deputy Julian in charge, who sees it as a perfect opportunity for him to finally make his mark on the place.";
-            showDetails.EpisodeDetails.Add(s01e02);
-
-            EpisodeDetails s01e03 = new EpisodeDetails();
-            s01e03.TitleExtra = "";
-            s01e03.Title = "s01e03 Trust The Trust";
-            s01e03.Description = "A mysterious visitor arrives. When he shows genuine interest in how the place is run, the team immediately suspect he has been sent by the Trust to spy on them.";
-            showDetails.EpisodeDetails.Add(s01e03);
-
-            EpisodeDetails s01e04 = new EpisodeDetails();
-            s01e04.TitleExtra = "";
-            s01e04.Title = "s01e04 Peter v Prynne";
-            s01e04.Description = "Tom has organised a special summer event and invited the great and good from the world of George Pudding scholarship. But with Peter encountering his arch nemesis - the unbearably pompous Mungo Prynne, Julian upsetting a neighbouring farmer and Emma rather enjoying the plum punch, will it be remembered for the right reasons?";
-            showDetails.EpisodeDetails.Add(s01e04);
-
-            EpisodeDetails s01e05 = new EpisodeDetails();
-            s01e05.TitleExtra = "";
-            s01e05.Title = "s01e05 The Rather Risky Ramble";
-            s01e05.Description = "Tom returns from a team building course in London to find the Plum House team have been bickering in his absence.";
-            showDetails.EpisodeDetails.Add(s01e05);
-
-            EpisodeDetails s01e06 = new EpisodeDetails();
-            s01e06.TitleExtra = "";
-            s01e06.Title = "s01e06 Lights, Camera, Pudding";
-            s01e06.Description = "The team at the museum are startled to hear that Peter has agreed to Plum House being used as a location in a period drama.";
-            showDetails.EpisodeDetails.Add(s01e06);
 
             EpisodeDetails s02e01 = new EpisodeDetails();
             s02e01.TitleExtra = "";
-            s02e01.Title = "s02e01 A Sound Investment";
-            s02e01.Description = "The team are making an audio tour they hope will bring visitors flocking in. But they can't agree on who will star in the recording, and Tom has a bigger problem when he accidentally breaks the most valuable artefact in the museum.";
+            s02e01.Title = "s02e01 Episode One";
+            s02e01.Description = "Can you change your sexual orientation at will? Is it ever fair to blame the children for a divorce? What happens if a therapist brings their own emotional baggage to the treatment room?";
             showDetails.EpisodeDetails.Add(s02e01);
 
             EpisodeDetails s02e02 = new EpisodeDetails();
             s02e02.TitleExtra = "";
-            s02e02.Title = "s02e02 Thief Encounter";
-            s02e02.Description = "Peter and Julian are determined to retrieve a valuable pudding artefact and they've enlisted the help of a crooked connection to do so. Emma and Tom try to keep everyone on the straight and narrow - but Maureen finds herself mysteriously drawn to the shady visitor.";
+            s02e02.Title = "s02e02 Episode Two";
+            s02e02.Description = "How can a tea-cosey help with dating decisions? What would football fans chant if they could express their true feelings? What are some of the do's and don'ts of role play?";
             showDetails.EpisodeDetails.Add(s02e02);
 
             EpisodeDetails s02e03 = new EpisodeDetails();
             s02e03.TitleExtra = "";
-            s02e03.Title = "s02e03 Black Pudding";
-            s02e03.Description = "The offer of an honorary degree at a less than prestigious ex-polytechnic sends Peter into a depressive spiral, as he questions whether he has wasted his life. The team call on the chief psychiatrist to the academic profession, the radical Doctor Bloch.";
+            s02e03.Title = "s02e03 Episode Three";
+            s02e03.Description = "Can families be merged like companies? Should ransom notes be aesthetically pleasing? Why do people think truffle hunting in Tuscany would be the ideal life?";
             showDetails.EpisodeDetails.Add(s02e03);
 
             EpisodeDetails s02e04 = new EpisodeDetails();
             s02e04.TitleExtra = "";
-            s02e04.Title = "s02e04 Death Comes To Plum House";
-            s02e04.Description = "Tom has organised a Murder Mystery event at Plum House. But when a storm prevents the visitors from reaching the museum, the team decide to try the game themselves. Maureen is determined to prove the murderer is Julian, while Peter is carried away with his new person - but a bloodcurdling scream from Alan means the team have a real mystery to solve.";
+            s02e04.Title = "s02e04 Episode Four";
+            s02e04.Description = "How would middle class liberals plan a jewellery heist? What are the categories for the new Modern Man Awards? Why are people in care homes forced into group singing?";
             showDetails.EpisodeDetails.Add(s02e04);
-
-            EpisodeDetails s02e05 = new EpisodeDetails();
-            s02e05.TitleExtra = "";
-            s02e05.Title = "s02e05 Hot Quiz";
-            s02e05.Description = "A local pub quiz becomes the arena to decide who is best and smartest at Plum House after Tom decides that Peter and Julian should no longer be in charge of the finances.";
-            showDetails.EpisodeDetails.Add(s02e05);
-
-            EpisodeDetails s02e06 = new EpisodeDetails();
-            s02e06.TitleExtra = "";
-            s02e06.Title = "s02e06 Wedding Bells";
-            s02e06.Description = "The team are preparing to host their first wedding at the museum. With a journalist coming to cover the event, it's a publicity coup for Plum House. But the groom has cold feet. Can they carry on regardless with a fake wedding? Tom is dubious - until Julian suggests he might pretend to marry Emma.";
-            showDetails.EpisodeDetails.Add(s02e06);
-
-            EpisodeDetails s03e01 = new EpisodeDetails();
-            s03e01.TitleExtra = "";
-            s03e01.Title = "s03e01 Better The Devil";
-            s03e01.Description = "With manager Tom off on leave, the arrival of Roger from Head Office - sent to reform the operation, is a shock to them all. How will they get rid of him and entice Tom back?";
-            showDetails.EpisodeDetails.Add(s03e01);
-
-            EpisodeDetails s03e02 = new EpisodeDetails();
-            s03e02.TitleExtra = "";
-            s03e02.Title = "s03e02 #SO BRITISH";
-            s03e02.Description = "The curator Peter Knight finds unlikely TV fame when he is featured on an ancestry show helping Derren Brown trace his roots to the museum.";
-            showDetails.EpisodeDetails.Add(s03e02);
-
-            EpisodeDetails s03e03 = new EpisodeDetails();
-            s03e03.TitleExtra = "";
-            s03e03.Title = "s03e03 A Knight to Remember";
-            s03e03.Description = "The Plum House team are once again invited to the Museum of the Year awards in London, leaving Julian in charge in Tom and Peter's absence.";
-            showDetails.EpisodeDetails.Add(s03e03);
-
-            EpisodeDetails s03e04 = new EpisodeDetails();
-            s03e04.TitleExtra = "";
-            s03e04.Title = "s03e04 Grace and Favour";
-            s03e04.Description = "Tom hopes to earn some funds for the museum by renting out Peter's apartment. Meanwhile there's love in the air for Tom and Julian, but not in the way either expects...";
-            showDetails.EpisodeDetails.Add(s03e04);
-
-            EpisodeDetails s03e05 = new EpisodeDetails();
-            s03e05.TitleExtra = "";
-            s03e05.Title = "s03e05 All in Good Faith";
-            s03e05.Description = "The team welcome a new vicar to their parish, and Maureen and Alan try in their own way to help with church repairs and maintenance, to disastrous effect. Meanwhile, Tom tries to patch things up with Emma.";
-            showDetails.EpisodeDetails.Add(s03e05);
-
-            EpisodeDetails s03e06 = new EpisodeDetails();
-            s03e06.TitleExtra = "";
-            s03e06.Title = "s03e06 Fete and Fortune";
-            s03e06.Description = "Plum House is hosting the annual Summer Fete for the area. Meanwhile Maureen's old nemesis Sue is out to get her...";
-            showDetails.EpisodeDetails.Add(s03e06);
 
             return showDetails;
         }
@@ -189,7 +75,8 @@ namespace ThumbnailCreator.Data
         // 1920 x 1040
         internal static ShowDetails PopulateShowData()
         {
-            return CenturyVampire();
+            return PleaseUseOtherDoor();
+            //return CenturyVampire();
             //            return PlumHouse();
             //return DeepStationEmerald();
             //return Bookcases();
