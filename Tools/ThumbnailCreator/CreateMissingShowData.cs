895fe7a0f0fe5e0572f16e404305b258a8e31eeb
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-07-26 17:25:33 +0100

    added the default ShowDetails

--- Tools/ThumbnailCreator/CreateMissingShowData.cs
+++ Tools/ThumbnailCreator/CreateMissingShowData.cs
@@ -1,11 +1,14 @@
 ﻿using eWolfAudioShows;
 using eWolfAudioShows.Interfaces;
 using System.IO;
+using ThumbnailCreator.Data;
 
 namespace ThumbnailCreator
 {
     internal class CreateMissingShowData
     {
+        private ShowDetails _showDetails;
+
         public void Do()
         {
             var shows = GetAll();
@@ -20,14 +23,39 @@ namespace ThumbnailCreator
                 if (File.Exists($"{path}\\1.png"))
                     continue;
 
-                int i = 0;
-                i++;
+                string image = $"{show.OutputPath}Ref\\{show.Title}.jpeg";
+                if (!File.Exists(image))
+                    continue;
+
+                SetShow(show);
             }
         }
 
         private static List<IAudioShow> GetAll()
         {
             return Holder.GetAll();
+        }
+
+        private void SetShow(IAudioShow show)
+        {
+            string path = $"{show.OutputPath}\\thumbnails\\";
+            string image = $"{path}\\Ref\\{show.Title}.jpeg";
+
+            ShowDetails showDetails = new ShowDetails();
+            showDetails.Source = image; // TODO: need to load the image.
+            showDetails.Title = show.Title;
+            showDetails.TitleLine2 = "";
+            showDetails.Path = show.OutputPath;
+            showDetails.Description = show.Description;
+            showDetails.ShowTypeLineA = "Radio";
+            showDetails.ShowTypeLineB = "Comedy";
+            showDetails.Series = show.Shows.Shows.Count();
+            showDetails.ShortShow = false;
+            showDetails.CompleteShow = false;
+            showDetails.ComdeyShow = true;
+            showDetails.ScifiShow = false;
+
+            _showDetails = showDetails;
         }
     }
 }
