aff5e35fa8fd5921dc45019f789283f24e6fadff
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-12-31 09:28:58 +0000

    Allow Title labe to be changed and show the outline text.

---
+++ Tools/Titles/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Titles
+{
+    public partial class MainWindow : Window
+    {
+        public SolidColorBrush _fillColor = new SolidColorBrush(Colors.Yellow);
+        private string _title = "Title";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SolidColorBrush FillColor
+        {
+            get
+            {
+                return _fillColor;
+            }
+            set
+            {
+                _fillColor = value;
+                OnPropertyChanged(nameof(FillColor));
+            }
+        }
+
+        public string TitleLabel
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(TitleLabel));
+                ShowTitle.Text = _title;
+                OnPropertyChanged(nameof(ShowTitle));
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Tools/Titles/WPF/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Titles.WPF
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Visible; // Default fallback
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+            return false; // Default fallback
+        }
+    }
+}
