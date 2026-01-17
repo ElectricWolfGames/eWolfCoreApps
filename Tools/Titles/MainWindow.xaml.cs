251788da287e70fbf7ceb313d4632b02066589a5
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-01-17 09:26:44 +0000

    Moved code in to the WPF common project.

--- Tools/Titles/MainWindow.xaml.cs
+++ Tools/Titles/MainWindow.xaml.cs
@@ -5,120 +5,119 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Titles.Helpers;
 
-namespace Titles
+namespace Titles;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    // nice font - Montserrat Subrayada
+
+    public SolidColorBrush _fillColor = new SolidColorBrush(Colors.Yellow);
+    private string _titleA = "Episode 2:";
+    private string _titleB = "Light and Track,\n\r+ Rolling Stock";
+
+    // 120 episode fton size.
+    public MainWindow()
     {
-        // nice font - Montserrat Subrayada
+        InitializeComponent();
 
-        public SolidColorBrush _fillColor = new SolidColorBrush(Colors.Yellow);
-        private string _titleA = "Episode 2:";
-        private string _titleB = "Light and Track,\n\rand new Rolling Stock";
+        DataContext = this;
+    }
 
-        // 120 episode fton size.
-        public MainWindow()
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public SolidColorBrush FillColor
+    {
+        get
         {
-            InitializeComponent();
-
-            DataContext = this;
+            return _fillColor;
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public SolidColorBrush FillColor
+        set
         {
-            get
+            _fillColor = value;
+            OnPropertyChanged(nameof(FillColor));
+        }
+    }
+
+    public string TitleLabel
+    {
+        get { return _titleA; }
+        set
+        {
+            _titleA = value;
+            OnPropertyChanged(nameof(TitleLabel));
+            ShowTitle.Text = _titleA;
+            OnPropertyChanged(nameof(ShowTitle));
+        }
+    }
+
+    public string TitleLabelB
+    {
+        get { return _titleB; }
+        set
+        {
+            _titleB = value;
+            OnPropertyChanged(nameof(TitleLabelB));
+            ShowTitleB.Text = _titleB;
+            OnPropertyChanged(nameof(ShowTitleB));
+        }
+    }
+
+    public static void CaptureScreen(UIElement source, Uri destination)
+    {
+        try
+        {
+            double height, renderHeight, width, renderWidth;
+
+            height = renderHeight = source.RenderSize.Height;
+            width = renderWidth = source.RenderSize.Width;
+
+            //Specification for target bitmap like width/height pixel etc.
+            RenderTargetBitmap renderTarget = new((int)renderWidth, (int)renderHeight, 0, 0,
+                PixelFormats.Pbgra32);
+
+            //creates Visual Brush of UIElement
+            VisualBrush visualBrush = new(source);
+
+            DrawingVisual drawingVisual = new();
+            using (DrawingContext drawingContext =
+            drawingVisual.RenderOpen())
             {
-                return _fillColor;
+                //draws image of element
+                drawingContext.DrawRectangle(visualBrush, null, new
+                Rect(new System.Windows.Point(0, 0), new System.Windows.Point(width, height)));
             }
-            set
-            {
-                _fillColor = value;
-                OnPropertyChanged(nameof(FillColor));
-            }
-        }
+            //renders image
+            renderTarget.Render(drawingVisual);
 
-        public string TitleLabel
+            //PNG encoder for creating PNG file
+            PngBitmapEncoder encoder = new();
+            encoder.Frames.Add(BitmapFrame.Create(renderTarget));
+            using FileStream stream = new
+                (destination.LocalPath, FileMode.Create,
+            FileAccess.Write);
+            encoder.Save(stream);
+        }
+        catch (Exception e)
         {
-            get { return _titleA; }
-            set
-            {
-                _titleA = value;
-                OnPropertyChanged(nameof(TitleLabel));
-                ShowTitle.Text = _titleA;
-                OnPropertyChanged(nameof(ShowTitle));
-            }
+            MessageBox.Show(e.ToString());
         }
+    }
 
-        public string TitleLabelB
-        {
-            get { return _titleB; }
-            set
-            {
-                _titleB = value;
-                OnPropertyChanged(nameof(TitleLabelB));
-                ShowTitleB.Text = _titleB;
-                OnPropertyChanged(nameof(ShowTitleB));
-            }
-        }
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        handler?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 
-        public static void CaptureScreen(UIElement source, Uri destination)
-        {
-            try
-            {
-                double height, renderHeight, width, renderWidth;
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        string basePath = "E:\\temp\\Titles\\";
+        Directory.CreateDirectory(basePath);
+        Uri path = new($"{basePath}\\title.png");
 
-                height = renderHeight = source.RenderSize.Height;
-                width = renderWidth = source.RenderSize.Width;
+        UIElement element = (UIElement)this.Content;
+        CaptureScreen(element, path);
 
-                //Specification for target bitmap like width/height pixel etc.
-                RenderTargetBitmap renderTarget = new((int)renderWidth, (int)renderHeight, 0, 0,
-                    PixelFormats.Pbgra32);
-
-                //creates Visual Brush of UIElement
-                VisualBrush visualBrush = new(source);
-
-                DrawingVisual drawingVisual = new();
-                using (DrawingContext drawingContext =
-                drawingVisual.RenderOpen())
-                {
-                    //draws image of element
-                    drawingContext.DrawRectangle(visualBrush, null, new
-                    Rect(new System.Windows.Point(0, 0), new System.Windows.Point(width, height)));
-                }
-                //renders image
-                renderTarget.Render(drawingVisual);
-
-                //PNG encoder for creating PNG file
-                PngBitmapEncoder encoder = new();
-                encoder.Frames.Add(BitmapFrame.Create(renderTarget));
-                using FileStream stream = new
-                    (destination.LocalPath, FileMode.Create,
-                FileAccess.Write);
-                encoder.Save(stream);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
-        }
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string basePath = "E:\\temp\\Titles\\";
-            Directory.CreateDirectory(basePath);
-            Uri path = new($"{basePath}\\title.png");
-
-            UIElement element = (UIElement)this.Content;
-            CaptureScreen(element, path);
-
-            WindowsHelper.OpenFileExplorer(basePath);
-        }
+        WindowsHelper.OpenFileExplorer(basePath);
     }
 }
