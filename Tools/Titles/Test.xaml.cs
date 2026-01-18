cc75c13cc3bbc78f51389d3622b0614b689934df
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-01-18 08:38:27 +0000

    Added test page with out of markers

---
+++ Tools/Titles/Test.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Titles;
+
+public partial class Test : Window
+{
+    public Test()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    public int Current { get; set; } = 1;
+    public int Total { get; set; } = 3;
+}
