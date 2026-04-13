e5df14a7afb7f70a8fb9a59878035731886e2f63
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-04-13 07:05:33 +0100

    Added some base files

---
+++ Tools/FilmTagger/IO/ReadFiles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmTagger.IO;
+
+internal class ReadFiles
+{
+}
+
+public class FileTagsTypes
+{
+}
---
+++ Tools/FilmTagger/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace FilmTagger;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Browser.Source = new Uri("https://en.wikipedia.org/wiki/No_Time_for_Sergeants_(film)");
+        // E:\_ToWatch\_ToCheck
+    }
+
+    private void AddTag(string tagName)
+    {
+        //
+    }
+
+    private void ButtonAction_Click(object sender, RoutedEventArgs e)
+    {
+        AddTag("Action");
+    }
+
+    private void ButtonComedy_Click(object sender, RoutedEventArgs e)
+    {
+        AddTag("Comedy");
+    }
+}
